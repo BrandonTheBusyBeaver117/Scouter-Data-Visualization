a29cc527ea95db1514cbde16315d0bbfbd8052e9
Brandon <25nguyenb26@stu.smuhsd.org>
2022-03-23 17:48:53 -0700

    Connected Front End to backend
    Click the button and now you can get data. 
    
    Future goals:
    Style, so it isn't ugly af
    In the future, I'm going to make this process run in the background, as well as making it more efficient

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,8 @@ const express = require('express')
 const axios = require('axios')
 
 const app = express()
-const PORT = process.env.PORT || 5000 //Default is 3000, which is the same as react, so changed to 5000 so there's no conflict
+const PORT = 5000 //Default is 3000, which is the same as react, so changed to 5000 so there's no conflict
 
-let teamRankings = [];// make this an object of all the teams and their rankings
-let responseSave;
 
 const headers = {
     'accept': 'application/json',
@@ -26,28 +24,13 @@ app.get('/', (req, res) => {
 app.get("/getData", (req, res) => {
     axios.get("https://www.thebluealliance.com/api/v3/event/"+ eventKey + "/rankings", {params: headers})
       .then(function(response) {
-        responseSave = response.data;
-        res.json(response.data)
-        console.log(responseSave)// printing out the json file
-        
-        for (let i = 0; i <responseSave.rankings.length; i++){
-            teamRankings.push({
-                "teamNumber" : responseSave.rankings[i].team_key.replace("frc",""),
-                "ranking" : responseSave.rankings[i].rank});
-       }     
-
-       /*
-       Ideas to make this more efficient:
-       Save a previous iteration of the json file, then constantly compare it to the current json file
-       Only if you see a difference, then you iterate through the entire array
-       then change the rest of the data on the site
-       */
-
-       console.log(teamRankings[35].teamNumber + "'s ranking is " + teamRankings[35].ranking)
+       //res.send(responseSave)//sends results?
+       
 
       }).catch(function(error) {
         res.json("Error occured!" + error)
       })
+      
   })
 
   // somehow save it all as an array and maybe map it later?
--- peek/src/TBA.js
+++ peek/src/TBA.js
@@ -1,26 +1,85 @@
 import React, {Component} from 'react';
 import axios from "axios";
 
+let teamRankings = [];
+let responseSave;
+let holdTeams;
+let newText;
+let idMaker;
+
 export class TBA extends Component {
+
     constructor() {
         super();
         this.state = {
-            team: "N/A"
+            rawTeam: "N/A",
+            allTeamsString : "Click me"
         }
+        this.getAllTeams = this.getAllTeams.bind(this);
+  
     }
 
-    componentDidMount = () => {
-        console.log('componentMounted');
+    componentDidMount = () => {        
         axios.get('/getData').then( response => {
-            this.state.team = response;
-        })
+            console.log(response)
+            
+       /*
+       Ideas to make this more efficient:
+       Save a previous iteration of the json file, then constantly compare it to the current json file
+       Only if you see a difference, then you iterate through the entire array
+       then change the rest of the data on the site
+       */      
+       responseSave = response.data;
+       //res.json(response.data)
+       console.log(responseSave)// printing out the json file
+       
+       for (let i = 0; i <responseSave.rankings.length; i++){
+           teamRankings.push({
+               "teamNumber" : responseSave.rankings[i].team_key.replace("frc",""),
+               "ranking" : responseSave.rankings[i].rank});
+      }     
+
+       console.log(teamRankings)
+       this.setState({rawTeam : teamRankings});
+        console.log(this.state.rawTeam);
+        
+       console.log(this.state)
+        }).catch(function(error) {
+           console.log("Error occured!" + error)
+          })
     }
     
+   getAllTeams() {
+       console.log("get")
+        for (let i = 0; i <this.state.rawTeam.length; i++){
+            
+        holdTeams+="Team #" + this.state.rawTeam[i].teamNumber + " is rank: " + this.state.rawTeam[i].ranking + "\n";
+
+    }
+
+    console.log(holdTeams)  
+    /*
+       newText = holdTeams.split ('\n').map ((item, iterate) => {
+        idMaker = "TeamList#" + iterate;
+       return <p key= {idMaker}>{item}</p>
+       
+       });
+       */
+       newText = holdTeams.split ('\n').map ((item, iterate) => <p key={iterate}>{item}</p>);
+console.log(newText[7])
+       this.setState({allTeamsString : newText});
+
+       {this.state.people.map((person, index) => (
+        <p>Hello, {person.name} from {person.country}!</p>
+    ))}
+
+    }
+
     render() {
         return (
         <div>
             <h1>These are the teams:</h1>
-            <p>{this.state.team}</p>
+            <button onClick={this.getAllTeams}>{this.state.allTeamsString}</button>
         </div>
         )
      
