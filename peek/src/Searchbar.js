986194fcfc3748574626d950d566b3ee23c882ad
Brandon <25nguyenb26@stu.smuhsd.org>
2022-05-31 14:41:04 -0700

    Made the searchbar slightly more efficient
    Deleted the "comparable characters" thing, and just compared the arrays directly. Also added comments before I forget

--- peek/src/Searchbar.js
+++ peek/src/Searchbar.js
@@ -18,10 +18,18 @@ export default function Searchbar (props) {
         //If not, I need to add class and its own scss file
         )
 
-        for (const item of props.teamData) {
-            listOfTotalTeams.push({
+        for (const item of props.teamData) { // This goes through all the elements in the teamData array
+
+            listOfTotalTeams.push({ //For each element, it pushes that a new team object to a list of all the teams
+
+                /*
+                 Property 1 is the team's team number 
+                 It's a string so it can be compared to the string output of the searchbar later
+                */
+
                 teamNumber: String(item[0]),
-                teamRanking: item[1]
+
+                teamRanking: item[1] // The rank of the team at competition
             
             })
         }
@@ -33,41 +41,69 @@ export default function Searchbar (props) {
                     //More research is needed for this...maybe make it state?
 
 
+
+    /*
+        When there's a change to the searchbar, this function will run
+
+        It takes in the event, which will be stored in "response"
+
+        It works to filter through the teams and output team results
+    */
+
     const handleChange = event => {
-        //Works to filter through teams
-        const response = [...event.target.value]; //Splits the response into an array of characters
+       
+        const response = [...event.target.value]; //Splits the searchbar response into an array of characters
 
-        const teamSearchResults = []// These are all the teams that the program has 
+        const teamSearchResults = []// These are all the teams that matches the response
 
-        if(response.length < 1) {
-            setSearchableTeamResults([])
-            console.log("no input")
-        }else if(/\d/.test(response)){ // this line tests whether the input string as numbers or not
+        if(response.length < 1) { // Checks if there actually is a response 
 
-            let anyResultFound = false;
+            setSearchableTeamResults([])// Says that there's no results
 
-            console.log("string: " + response + "length: " + response.length)
+            //console.log("no input")
+        }else if(!(/[a-z]/i.test(response))){ // Makes sure that the searchbar input doesn't have letters
+
+            let anyResultFound = false; // Were there any results found?
+
+            //console.log("string: " + response + "length: " + response.length)
+
+
+            /*
+                Goes through all the teams and compares it to Search Input
+            */
             for ( let teamIndex = 0; teamIndex < listOfTotalTeams.length; teamIndex++) {
 
                 const team = listOfTotalTeams[teamIndex];//Defining the team in this iteration
 
-                //Here's where sorting algorithms come into play
-               
                 const arrayTeamNumber = [...team.teamNumber] //Splits the team number into an array of characters
 
-                const comparableDigits = [] // These digits are 
-
-
-                for ( let i = 0; i < response.length; i++){
-                    comparableDigits.push(arrayTeamNumber[i])                   
-                }
+                /*
+                    For each iteration, it starts off with 0 character matches between th e
+                    search input and the team number
+                */
+                let successfulCharacterMatches = 0; 
                 
-                let successfulCharacterMatches = 0;
+
+                /*
+                    Goes through all the characters in the response and compares it to the first 
+                    same amount of characters in the team number 
+                    
+                    If a match is found, add 1 to the successful matches
+                */
                 for(let i = 0; i < response.length; i++) {
-                    if(response[i] == comparableDigits[i]){
+
+                    if(response[i] == arrayTeamNumber[i]){
                         successfulCharacterMatches++;
                     }
                 }
+
+
+                /*
+                    If all the compared characters are equal, then a result is found
+                    Make sure we have found at least 1 match, and set anyResult to true (That's important later)
+                    
+                    Then, add the found team's <p> result element to the "team search results" array
+                */
 
                if(successfulCharacterMatches === response.length){ // Basically saying if all the characters check out, then put the search
                                                             //I wonder if there's a way to make it more efficient
@@ -80,10 +116,12 @@ export default function Searchbar (props) {
                     teamSearchResults.push(totalTeamsComponent[teamIndex])
                }
 
-               if(anyResultFound){
+
+               if(anyResultFound){// If there's any results found set the state, "searchable team results", to those results
                 setSearchableTeamResults(teamSearchResults)
 
                } else {
+                   //If there's no result found, then the state will just be plain text that says the following message
                    setSearchableTeamResults(["No teams found with that number"])
                }
     
