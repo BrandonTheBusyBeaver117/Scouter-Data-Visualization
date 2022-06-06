7f2384fbcad0086ec8c52e5fd98f4a0b068f65e8
Brandon <25nguyenb26@stu.smuhsd.org>
2022-06-05 20:04:13 -0700

    Adding some styles and comments to team component
    Added comments to improve readability to other programmers. Also added some little style changes. Going to fix the wide column between the team component and the left side of the screen, that's due to the context menu.

--- peek/src/Team.js
+++ peek/src/Team.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import "./Team.scss"
 
 export class Team extends Component{
-//among us is sus 
+
     /*Basically this class should hold all the data
     Makes it searchable, call methods etc.
     Idk if we should update data in this class then pass
@@ -40,36 +41,64 @@ export class Team extends Component{
         
         
         
-        //--------------------
+        /*------------------------------------------------------------------------------
+
+        prop.teamData is all the data for the team
+        structure is laid out below, but might be subject to change year to year
+
+        prop.teamData[0] is the team number
+        prop.teamData[1] is the team ranking 
+
+        Every index afterwards contains an individual match that this team performed in
+        prop.teamData[2 --> infinity]
+        This is why in the for loop down below, i starts off as 2, because the "individual match data" starts at index 2
+        
+        */
 
         //individual match
-        let data = [];
+        let allMatchData = [];
+        console.log(this.props.teamData)
         for(let i = 2; i < this.props.teamData.length; i++ ) {// 2 represents the index where the matches actually start
-            let individualMatchData = []
-            const individualMatch = this.props.teamData[i]
-            for(let t = 2; t < individualMatch.length; t++){//1 represents the match data, skipping past version number and team Num
 
-                individualMatchData.push(<td>{individualMatch[t]}</td>)
+            //Iterates through all the matches, for each iteration,  
+            
+            let refinedMatchData = []
+            
+            const rawIndividualMatch = this.props.teamData[i]
+
+            for(let t = 2; t < rawIndividualMatch.length; t++){
+                /*
+                    2 represents the match data, skipping past version number (index 0) and team Num (index 1)
+                    We basically cut off the first two indexes of the raw individual match
+                    Then, we save the new data in refinedMatchData
+                */
+
+                refinedMatchData.push(<td>{rawIndividualMatch[t]}</td>)
 
             }
-            data.push(<tr>{individualMatchData}</tr>)
+            
+            /*
+                Now, with the refined match data, we save that as a single row in allMatchData
+                In the end, allMatchData will have all the refined matches, each as a separate row
+            */
+
+            allMatchData.push(<tr>{refinedMatchData}</tr>)
         }
         
 
+        //The final table to be rendered
         const saveTable = 
         <table>
             <thead>
             {googleSheetHeaders}
             </thead>
             <tbody>
-            {data}
+            {allMatchData}
             </tbody>
         </table>
 
 
-        this.setState({
-            table: saveTable
-        })
+        this.setState({table: saveTable})// Sets state to the table
 
     }
 
@@ -77,7 +106,7 @@ export class Team extends Component{
         //this is where the team blocky thing should be rendered
        
         return(
-        <div>
+        <div className='teamComponent'>
             <h1>{this.props.teamData[0]}</h1>
             <h2>Rank: {this.props.teamData[1]}</h2>
             
