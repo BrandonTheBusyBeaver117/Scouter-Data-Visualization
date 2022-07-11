af07d47b552a156ae2dad549f27bebf5fb65ef71
Brandon <25nguyenb26@stu.smuhsd.org>
2022-07-10 17:09:26 -0700

    Getting teamHolder to be not part of state
    The user should have control over what teams they want to see. Therefore, teamHolder doesn't need to be part of state. Chosen teams just needs to access teamHolder to get the teams. Only then do the teams need to rerender

--- peek/src/TeamModifier.js
+++ peek/src/TeamModifier.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import {Team} from './Team_Component/Team'
+import { Team } from './Team_Component/Team'
 import { ContextMenu } from './ContextMenu';
 import Searchbar from './Searchbar';
 import SideMenu from "./SideMenu";
 import "./TeamModifier.scss";
 
 
-import axios from "axios";  
+import axios from "axios";
 
 
 let matchData = []
+let teamData = []
+let teamHolder = []
 
 export class TeamModifier extends Component {
 
@@ -17,26 +19,26 @@ export class TeamModifier extends Component {
     constructor() {
         super();
         this.state = {
-             
+
             googleSheetHeaders: "n/a",
             teamData: [],
-            teamHolder: "",
+            chosenTeams: [],
 
             //Individual year variables
 
-            teamColumn: 1, //Column (in array notation) where team number is defined, in case it (for whatever reason) changes year to yea
+            teamColumn: 1, //Column (in array notation) where team number is defined, in case it (for whatever reason) changes year to year
 
             // Context menu
             toggleMenu: false,
             xPositionOfContextMenu: 0,
             yPositionOfContextMenu: 0,
-            clicked: false, 
+            clicked: false,
         }
 
         this.toggleMenu = this.toggleMenu.bind(this)
     }
 
-    componentDidMount = () => { 
+    componentDidMount = () => {
         console.log("mounted")
         this.createTeams()
 
@@ -44,23 +46,26 @@ export class TeamModifier extends Component {
 
 
 
-    toggleMenu (isToggled, mouseX, mouseY, isClicked) {
-     
+    toggleMenu(isToggled, mouseX, mouseY, isClicked) {
+
         console.log(isToggled + mouseX + mouseY)
         this.setState({
             toggleMenu: isToggled,
             xPositionOfContextMenu: mouseX,
-            yPositionOfContextMenu: mouseY, 
+            yPositionOfContextMenu: mouseY,
             clicked: isClicked
         })
     }
-   
-        
-    
-    getTBAData() { 
 
-        return axios.get('/getData').then( response => {
-            
+
+    setTeamData(newData) {
+        teamData = [...newData]
+    }
+
+    getTBAData() {
+
+        return axios.get('/getData').then(response => {
+
             /*
             Ideas to make this more efficient:
             Save a previous iteration of the json file, then constantly compare it to the current json file
@@ -68,146 +73,179 @@ export class TeamModifier extends Component {
             then change the rest of the data on the site
 
             This might only be useful for google sheets however
-            */      
-            const responseSave = response.data.rankings; 
-            
-            for (const Team of responseSave){
-                this.state.teamData.push([
-                    Team.team_key.replace("frc",""),//gets rid of the "frc" before the number
-                    Team.rank]);
-            }//This is basically a for-each loop. It iterates through the entire response data array and saves the rankings     
+            */
 
-            /*maybe return teamrankings, or just use objectappend to combine 
+            const newData = []
+
+            for (const team of response.data.rankings) {
+
+                newData.push([
+                    Number(team.team_key.replace("frc", "")),//gets rid of the "frc" before the number
+                    team.rank]);
+            }//This is basically a for-each loop. It iterates through the entire response data array and saves the rankings     
+            this.setTeamData(newData)
+            /*maybe return team rankings, or just use objectappend to combine 
             the two arrays with spreadsheets and make one big object or something            
             */
-           console.log("Tba gotten")
-                }).catch(error => {
-                console.log("Error occured!" + error)
-                })
+            console.log("TBA data received")
+        }).catch(error => {
+            console.log("Error occured!" + error)
+        })
     }
 
-    getSpreadsheetData () {
-       return axios.get('/getSpreadsheetData').then( response => {  
 
-        //Checks if the headers are the same as before
-        //Only if they're different do they change
-        //Although, this may not be needed, or a better comparison might be needed
+    setMatchData(newMatches) {
+        matchData = [...newMatches]
+    }
 
-        const responseHeaders = response.data[0];
+    getSpreadsheetData() {
+        return axios.get('/getSpreadsheetData').then(response => {
 
-        if(responseHeaders.length !== this.state.googleSheetHeaders.length) {
+            //Checks if the headers are the same as before
+            //Only if they're different do they change
+            //Although, this may not be needed, or a better comparison might be needed
 
-            this.setState({googleSheetHeaders : responseHeaders})
-            console.log("headers are set")
+            const responseHeaders = response.data[0];
 
-        } else {
+            if (responseHeaders.length !== this.state.googleSheetHeaders.length) {
 
-            for(let i = 0; i < responseHeaders.length; i++) {
-                
-                if(responseHeaders[i] !== this.state.googleSheetHeaders[i]){
-                    this.setState({googleSheetHeaders : responseHeaders})
-                    console.log("headers are set")
+                this.setState({ googleSheetHeaders: responseHeaders })
+                console.log("headers are set")
+
+            } else {
+
+                for (let i = 0; i < responseHeaders.length; i++) {
+
+                    if (responseHeaders[i] !== this.state.googleSheetHeaders[i]) {
+                        this.setState({ googleSheetHeaders: responseHeaders })
+                        console.log("headers are set")
+
+                    }
 
                 }
-                
+
             }
 
-        }
-
-            matchData = response.data;  
+            this.setMatchData(response.data)
 
             this.parseThroughSpreadSheetData()
         }).catch(error => console.log(error))
 
-        
+
     }
 
-    parseThroughSpreadSheetData () {
+    parseThroughSpreadSheetData() {
 
-        for(const Match of matchData){// Gets the individual match data
-           let teamNumberFound = false;
-            for(let i = 0; i < this.state.teamData.length; i++){//Iterates through all previous data
-                
-                if(Match[this.state.teamColumn] === (this.state.teamData[i])[0]){//Checks if the Team already exists
-                    
-                    
-                    let alteredTeamData = this.state.teamData
+        let alteredTeamData = [...teamData] //is this a shallow copy??
+        console.log("Heres the team data: ")
+
+        for (const Match of matchData) {// Gets the individual match data
+            let teamNumberFound = false;
+            console.log((teamData[2])[0])
+            console.log(Match[1])
+            for (let i = 0; i < teamData.length; i++) {//Iterates through all previous data
+
+                if (Number(Match[this.state.teamColumn]) === (teamData[i])[0]) {//Checks if the Team already exists
+
+
                     alteredTeamData[i].push(Match)
-                    
+
                     teamNumberFound = true; // Says that this match was successfully found
- 
+
+                    /*
                     this.setState({
                         teamData : alteredTeamData
                     })
-                    
-                }  
+                    */
+
+                    break
+                }
 
             }
 
-            if(!teamNumberFound){
+            if (!teamNumberFound) {
                 console.log("uh oh, team: \"" + Match[this.state.teamColumn] + "\" does not exist")
             } // Checking if this match's team number was valid
-            
-        } 
+
+        }
+        this.setTeamData(alteredTeamData)
     }
 
 
-    getData () {
+    getData() {
 
-        return new Promise ( (resolve) => {
-            this.getTBAData().then( () => {
-        
-            this.getSpreadsheetData().then ( () => resolve(true))
-            //lol this is so cursed 
-            //Basically, we get TBA data first
-            //Only then do we get spreadsheetdata
-            //then, when both of those finish, we resolve
+        return new Promise((resolve) => {
+            this.getTBAData().then(() => {
+
+                this.getSpreadsheetData().then(() => resolve(true))
+                //lol this is so cursed 
+                //Basically, we get TBA data first
+                //Only then do we get spreadsheetdata
+                //then, when both of those finish, we resolve
+            })
+
         })
 
-    })
-
 
     }
 
-    async createTeams () {
+
+    //This is an array that holds all the teams
+    setTeamHolder (arrayOfTeams) {
+        teamHolder = [...arrayOfTeams]
+    }
+    async createTeams() {
 
         console.log("waiting...")
 
-        await this.getData().then( () =>{
+        await this.getData().then(() => {
 
-            console.log("MAKING TEAMS")   
-        
-                let teamArray= this.state.teamData.map((item,iterate) => <Team key = {iterate} 
-                googleSheetHeaders = {this.state.googleSheetHeaders}
-                teamData = {item}
-                toggleMenu = {this.toggleMenu}
-                />)
+            console.log("MAKING TEAMS")
 
+            let teamArray = teamData.map((item, iterate) => <Team key={iterate}
+                googleSheetHeaders={this.state.googleSheetHeaders}
+                teamData={item}
+                toggleMenu={this.toggleMenu}
+            />)
+
+            this.setTeamHolder(teamArray)
+
+            //Testing whether all teams will appear
+            const testingAllTeams = true
+            if (testingAllTeams){
                 
-                this.setState({teamHolder: teamArray})
+                this.setState({chosenTeams: teamHolder})
+            }
 
-        }).catch((error) =>{
+        }).catch((error) => {
             console.log(error)
         })
     }
 
 
 
+    setChosenTeams(newTeamArray) {
+        this.setState({chosenTeams: [...newTeamArray]})
+    }
+
+    getChosenTeams() {
+        return this.state.chosenTeams
+    }
 
     render() {
         return (
             <div>
-            <Searchbar teamData = {this.state.teamData}/>
-            <SideMenu />
-            <ContextMenu 
-                menuToggled = {this.state.toggleMenu} 
-                mouseX = {this.state.xPositionOfContextMenu} 
-                mouseY = {this.state.yPositionOfContextMenu}
-                clicked = {this.state.clicked}
-            />
+                <Searchbar teamData={teamData} />
+                <SideMenu />
+                <ContextMenu
+                    menuToggled={this.state.toggleMenu}
+                    mouseX={this.state.xPositionOfContextMenu}
+                    mouseY={this.state.yPositionOfContextMenu}
+                    clicked={this.state.clicked}
+                />
 
-            <div id = "Teams">{this.state.teamHolder}</div>
-        
+                <div id="Teams">{this.state.chosenTeams}</div>
+
             </div>
         )
 
