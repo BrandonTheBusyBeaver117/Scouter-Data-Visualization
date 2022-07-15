3ee58588a91b53c98ed73131134c4196542b7800
Brandon <25nguyenb26@stu.smuhsd.org>
2022-07-14 22:55:33 -0700

    Getting ready to sort teams
    Now I've made a (hash)map of the team qualities. In the future, I might search the map (based on the qualities as seen in the method below)

--- peek/src/TeamModifier.js
+++ peek/src/TeamModifier.js
@@ -141,8 +141,7 @@ export class TeamModifier extends Component {
 
         for (const Match of matchData) {// Gets the individual match data
             let teamNumberFound = false;
-            console.log((teamData[2])[0])
-            console.log(Match[1])
+            
             for (let i = 0; i < teamData.length; i++) {//Iterates through all previous data
 
                 if (Number(Match[this.state.teamColumn]) === (teamData[i])[0]) {//Checks if the Team already exists
@@ -202,6 +201,70 @@ export class TeamModifier extends Component {
 
             console.log("MAKING TEAMS")
 
+
+/*
+    Look into object fromEntries, which creates object from map
+    Somehow need to convert array of: [TeamNum, Ranking, [Data]] (or something, I can't remember)
+    To a map of header, [data for header] and so forth
+    The team num and rank would have to be stored elsewhere
+*/
+            console.log(teamData)
+            // A serious consideration is making teamholder a map...?
+            // That way, we can search...wait does that lead to any effencies?
+            const arrayOfMaps = []
+            const initialTeamMap = new Map()
+            //Initalizing Map with the keys, but empty values to be filled in later
+            for (const header of this.state.googleSheetHeaders){
+                initialTeamMap.set(header, [])
+            }
+            for (const team of teamData){
+                const newTeamMap = new Map(initialTeamMap)
+                console.log(team)
+                newTeamMap.set("teamNumber", team[0])
+                newTeamMap.set("teamRank", team[1])
+
+                const matches = team.slice(2) // skipping over teamNum and ranking
+                console.log(matches)
+                /*
+                    something to note here is that if there was a null value 
+                    (like somone leaving the comment's block blank), this script 
+                    would not just add anything to the key list
+                    in that way, you cannot map these data points to matches for sure
+                    unless you're really certain that thre's no null value
+                    
+                    one easy way to fix this is to, in the scouting app, set a default 
+                    response for every criteria (which I might do)
+
+                */
+
+                /*
+                    One thing to consider is the types of data there will be
+                    There'll be:
+                        * Team Number
+                        * Ranking
+                        * Version of Scouting App,
+                        * Comments
+                        * Quantitative Data (how much of something) (we can run statistics on these data points)
+                        * Boolean? Data (Whether a robot did something or not)
+
+
+                */
+
+                for(const match of matches){
+                    for(let i = this.state.teamColumn; i < match.length; i++){// We start when the Team number starts
+                        const key = this.state.googleSheetHeaders[i] // The header will be the key to the map
+                       
+                        const oldValue = newTeamMap.get(key)// Getting the old value for this particular key in the map
+                       
+                        newTeamMap.set(key, [...(oldValue), match[i]])
+                        
+                    } 
+                }
+                
+                arrayOfMaps.push(newTeamMap)
+            }
+            console.log(arrayOfMaps)
+
             let teamArray = teamData.map((item, iterate) => <Team key={iterate}
                 googleSheetHeaders={this.state.googleSheetHeaders}
                 teamData={item}
@@ -226,6 +289,10 @@ export class TeamModifier extends Component {
 
     setChosenTeams(newTeamArray) {
         this.setState({chosenTeams: [...newTeamArray]})
+    }
+
+    sortTeams(quality){
+        
     }
 
     getChosenTeams() {
