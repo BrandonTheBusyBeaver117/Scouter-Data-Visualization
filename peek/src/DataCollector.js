4d5a1f1fc9cdd5b5e7edacc351553f7068eee9df
Brandon <25nguyenb26@stu.smuhsd.org>
2022-09-17 09:59:31 -0700

    Compartmentalization of data retrieving
    Get data was so large, I made a new class called Data collector to make Team Modifier not as large.  Cleaned up some of the promises and next step should be actually implementing it in Team Modifier.

---
+++ peek/src/DataCollector.js
@@ -0,0 +1,143 @@
+import axios from "axios";
+
+
+export default class DataGetter {
+
+    constructor () {
+        this.teamData = []
+        this.matchData = []
+        this.googleSheetHeaders = "N/A"
+
+    }
+
+    getTeamData () {
+        return this.teamData;
+    }
+
+    getmatchData () {
+        return this.matchData;
+    }
+
+    getTeamData () {
+        return this.teamData;
+    }
+
+    getData() {
+
+        
+        // First get the teamData from The Blue Alliance
+        // Then go to our spreadsheeet and read data from there
+        return this.getTBAData()
+                .then(() =>  this.getSpreadsheetData())
+                .catch(error => console.log(error))
+    
+
+    }
+
+
+    
+getTBAData() {
+
+    return axios.get('/getData').then(response => {
+
+        /*
+        Ideas to make this more efficient:
+        Save a previous iteration of the json file, then constantly compare it to the current json file
+        Only if you see a difference, then you iterate through the entire array
+        then change the rest of the data on the site
+
+        This might only be useful for google sheets however
+        */
+
+        const newData = []
+
+        for (const team of response.data.rankings) {
+
+            newData.push([
+                Number(team.team_key.replace("frc", "")),//gets rid of the "frc" before the number
+                team.rank]);
+        }//This is basically a for-each loop. It iterates through the entire response data array and saves the rankings     
+
+        /*maybe return team rankings, or just use objectappend to combine 
+        the two arrays with spreadsheets and make one big object or something            
+        */
+        console.log("TBA data received")
+        
+        this.teamData = [...newData]
+        
+    }).catch(error => {
+        console.log(error)
+    })
+}
+
+ getSpreadsheetData() {
+    return axios.get('/getSpreadsheetData').then(response => {
+
+        //Checks if the headers are the same as before
+        //Only if they're different do they change
+        //Although, this may not be needed, or a better comparison might be needed
+
+        const responseHeaders = response.data[0];
+
+        if (responseHeaders.length !== this.state.googleSheetHeaders.length) {
+
+            this.googleSheetHeaders = responseHeaders
+            console.log("headers are set")
+
+        } else {
+
+            for (let i = 0; i < responseHeaders.length; i++) {
+
+                if (responseHeaders[i] !== this.state.googleSheetHeaders[i]) {
+                    this.googleSheetHeaders = responseHeaders
+                    console.log("headers are set")
+
+                }
+
+            }
+
+        }
+
+        this.matchData = response.data
+
+        this.parseThroughSpreadSheetData()
+    }).catch(error => console.log(error))
+
+}
+
+ parseThroughSpreadSheetData() {
+
+    let alteredTeamData = this.teamData.slice()
+    console.log("Here's the team data: ")
+
+    for (const Match of this.matchData) {// Gets the individual match data
+        let teamNumberFound = false;
+        
+        for (const [index, team] of this.teamData.entries()) {//Iterates through all previous data
+
+            if (Match[this.state.teamColumn] == team[0]) {//Checks if the Team already exists 
+
+
+                alteredTeamData[index].push(Match)
+
+                teamNumberFound = true; // Says that this match was successfully found
+
+        
+                break
+            }
+
+        }
+
+        if (!teamNumberFound) {
+            console.log("uh oh, team: \"" + Match[this.state.teamColumn] + "\" does not exist")
+        } // Checking if this match's team number was valid
+
+    }
+    this.teamData = alteredTeamData
+    }
+    
+}
+
+
+    
+
