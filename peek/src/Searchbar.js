f970a2dfb703c316116546f1768e47c2dde20352
Brandon <25nguyenb26@stu.smuhsd.org>
2022-11-24 10:39:24 -0800

    Further improvements to searchbar
    Made it even better. Made the searching aspect more efficient. Stopped looping. Got rid of some unneeded useEffects and made handleResultClick simpler

--- peek/src/Searchbar.js
+++ peek/src/Searchbar.js
@@ -10,30 +10,18 @@ export default function Searchbar(props) {
     // Allows chosen teams values to persist 
     const prevChosenTeams = useRef(props.chosenTeams)
 
-    const handleResultClick = newTeam => {
+    const handleResultClick = (newTeam) => {
 
-        console.log(props.chosenTeams)
-        console.log(newTeam)
-
-        const noDoubles = (previousTeams, newTeam) => {
-            if (previousTeams.includes(newTeam)) {
-                return [...previousTeams]
-            } else {
-                return [...previousTeams, newTeam]
-            }
+        if (prevChosenTeams.current.includes(newTeam)) {
+            props.setChosenTeams([...prevChosenTeams.current])
+        } else {
+            props.setChosenTeams([...prevChosenTeams.current, newTeam])
         }
-
-        const newArray = noDoubles(prevChosenTeams.current, newTeam)
-        props.setChosenTeams(newArray)
-
-        console.log(newArray)
 
     }
 
 
-
-
-    // Thanks to Ben Bud
+    // Thanks to Ben Bud's answer for inspiration
     // Solution found at https://stackoverflow.com/a/42234988
 
     const lastClickedClassName = useRef("")
@@ -56,6 +44,7 @@ export default function Searchbar(props) {
         }
         // Bind the event listener
         document.addEventListener("mousedown", handleClickOutside);
+
         return () => {
             // Unbind the event listener on clean up
             document.removeEventListener("mousedown", handleClickOutside);
@@ -64,21 +53,9 @@ export default function Searchbar(props) {
 
     // Handle chosen team update
     useEffect(() => {
-
-        console.log("New teams")
-        // Change the string passed into this handleChange if you want the searchbar to disappear
-        // I.e. make it ""
-        handleChange(searchbarValue, props.chosenTeams)
         prevChosenTeams.current = props.chosenTeams
+
     }, [props.chosenTeams])
-
-
-    // Handle SearchbarUpdate
-    useEffect(() => {
-        console.log(searchbarValue)
-        handleChange(searchbarValue, props.chosenTeams)
-    }, [searchbarValue])
-
 
 
     /**
@@ -88,72 +65,40 @@ export default function Searchbar(props) {
     */
     const handleChange = (allTeamsMap, inputValue, chosenTeams, isClickedInsideSearchbar) => {
 
-        console.log(inputValue)
+        //console.log(inputValue)
 
-        if (inputValue.length < 1 || !isClickedInsideSearchbar) { // Checks if there actually is a response 
-
-            return []// Says that there's no results
-
-            //console.log("no input")
+        // Is there a response? OR The user has clicked outside the searchbar
+        if (inputValue.length < 1 || !isClickedInsideSearchbar) {
+            // Show no results
+            return []
         }
 
-        const response = [...inputValue]; //Splits the searchbar response into an array of characters
+        // All the teams that matches the response
+        const teamSearchResults = []
 
-        const teamSearchResults = []// These are all the teams that matches the response
-
+        // All the teams that we grab from the map
         const allTeams = allTeamsMap.keys()
 
-        if (!(/[a-z]/i.test(response))) { // Makes sure that the searchbar input doesn't have letters
+        // Makes sure that the searchbar input doesn't have letters
+        if (!(/[a-z]/i.test(inputValue))) { 
 
-            let anyResultFound = false; // Were there any results found?
-
-            console.log("string: " + response + "length: " + response.length)
-
-
-            /*
-                Goes through all the teams and compares it to Search Input
-            */
+            // Were there any results found?
+            let anyResultFound = false; 
+        
+            // Traverses through all the team numbers
             for (const teamNumber of allTeams) {
 
-                const arrayTeamNumber = [...String(teamNumber)] //Splits the team number into an array of characters          
+                // .slice takes the length of the teamnumber to compare to the input value
+                // If they are equal, then it's a match, and it's a possible result
+                if (String(teamNumber).slice(0, inputValue.length) === inputValue) {
 
-                /*
-                    For each iteration, it starts off with 0 character matches between th e
-                    search input and the team number
-                */
-                let successfulCharacterMatches = 0;
-
-
-                /*
-                    Goes through all the characters in the response and compares it to the first 
-                    same amount of characters in the team number 
-                    
-                    If a match is found, add 1 to the successful matches
-                */
-                for (let i = 0; i < response.length; i++) {
-
-                    if (response[i] == arrayTeamNumber[i]) {
-                        successfulCharacterMatches++;
-                    }
-                }
-
-
-                /*
-                    If all the compared characters are equal, then a result is found
-                    Make sure we have found at least 1 match, and set anyResult to true (That's important later)
-                    
-                    Then, add the found team's <p> result element to the "team search results" array
-                */
-
-                if (successfulCharacterMatches === response.length) { // Basically saying if all the characters check out, then put the search
-                    //I wonder if there's a way to make it more efficient
-                    //Like, somehow eliminate choices as the program searches
-                    /*
-                    Is it more efficient to make a new <p> or result element every time I find a match
-                    or just iterate through another list full of all those premade elements
-                    */
+                    // We did find a result
                     anyResultFound = true;
+
+                    // Only push it if it's not already included
                     if (!chosenTeams.includes(teamNumber)) {
+                        
+                        // Push the result
                         teamSearchResults.push(<p
                             key={teamNumber}
                             onClick={() => handleResultClick(teamNumber)}
